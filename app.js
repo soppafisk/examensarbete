f403b57dd3ae4cb29f4337bc43f5a1b84f78358a
Jens <soppafisk@gmail.com>
2015-09-16 09:42:24 +0200

    blabla

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/plnk');
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });

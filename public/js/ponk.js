78697313f46e21ef9b5d4ae8e53b18610eecdc81
Jens <hej@jenserik.se>
2015-09-24 12:16:14 +0200

    deleteknapp

--- public/js/ponk.js
+++ public/js/ponk.js
@@ -2,17 +2,20 @@ var ponk = angular.module("ponk", []);
 
 ponk.controller("AppCtrl", ["$scope", function($scope) {
 
+  //dummy data
   $scope.modules = [
     {
       type: "text",
-      content: "hejhejhej"
+      content: "this is a text field"
     },
     {
       type: "youtube",
-      content: "aasfaölk"
+      content: "adress to youtubevideo"
     }
     ];
+
     $scope.showAddModule = false;
+
     $scope.newModule = {
       type: "text",
       content: ""
@@ -23,27 +26,54 @@ ponk.controller("AppCtrl", ["$scope", function($scope) {
   };
 
   $scope.addModule = function() {
-    $scope.modules.push($scope.newModule);
-    $scope.newModule = { type: "text", };
-    $scope.showAddModule = false;
-  }
+      $scope.modules.push($scope.newModule);
+      $scope.newModule = { type: "text", };
+      $scope.showAddModule = false;
+  };
+
+  $scope.deleteModule = function(module) {
+      var index = $scope.modules.indexOf(module);
+      if(index != -1) {
+        $scope.modules.splice(index);
+        console.log("splice");
+      } else {
+        console.log("nosplice");
+      }
+      console.log($scope.modules);
+  };
+
 }]);
 
 ponk.directive("module", function($compile) {
+  var before = '<div><span class="glyphicon glyphicon-remove-circle pointer" ng-click="deleteModule(module)"></span></div>';
+  var textTemplate = '<div class=" col-xs-2">' + before + '<div class="module">{{ module.content }}</div></div>';
+  var youtubeTemplate = '<div class="col-xs-4">' + before + '<div class="module"><iframe id="ytplayer" type="text/html" width="500" height="300" src="http://www.youtube.com/embed/?autoplay=0" frameborder="0"/></div></div>';
 
+  var getTemplate = function(type) {
+
+    var template = "";
+    switch(type) {
+      case "youtube":
+        template = youtubeTemplate;
+        break;
+      case "text":
+      default:
+        template = textTemplate;
+        break;
+    }
+    return template;
+  }
 
   var linker = function(scope, element, attrs) {
-    var getTemplate = function(type) {
-
-    }
-    getTemplate(scope.module.type);
-    console.log(element);
-    element.innerHTML = "hej";
+    var el = $compile(getTemplate(scope.module.type))(scope);
+    element.replaceWith(el);
   }
 
   return {
+    scope: {module: '=',},
+    transclude: 'true',
+    controller: 'AppCtrl',
     restrict: 'E',
-    //template: '<div class="module">{{ module.content }}</div>',
     link: linker,
   }
 });

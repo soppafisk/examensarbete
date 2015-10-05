0c26c16ce03a88364efb163d0e00ba96fb9ac94c
Jens <soppafisk@gmail.com>
2015-10-05 15:14:58 +0200

    börjat med databasanslutning

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ var path = require('path');
 var mongoose = require('mongoose');
 var app = express();
 
+mongoose.connect("mongodb://localhost/ponk");
+var Schema = mongoose.Schema;
+
+var boardSchema = new Schema({
+  link: String,
+  createdAt: {type: Date, default: Date.now},
+  widgets: [{wType: String, content: String, size: String, x: Number, y: Number}],
+});
+
+var Board = mongoose.model("Board", boardSchema);
+
 app.use(express.static(__dirname + "/public"));
 
 app.use('/vendor', express.static(__dirname + '/node_modules'));
@@ -13,12 +24,23 @@ app.get('/', function (req, res) {
 
 });
 
-var Schema = mongoose.Schema;
-
-var boardSchema = new Schema({
-  link: String,
-  widgets: [{type: String, content: String, size: String, x: Number, y: Number}],
+app.get('/board', function(req, res) {
+  Board.find(function(err, data) {
+    res.json(data);
+  })
 });
+
+app.post('/board', function(req, res) {
+  console.log(req);
+  var board1 = new Board({link: "ny länk", createdAt: new Date(),
+    widgets: [{wType: "text", content: "content2", size: "2x4", x: 1, y: 2}],
+  });
+  board1.save(function (err) {
+    if (err)
+      console.error(err);
+  });
+});
+
 
 
 
--- public/js/ponk.js
+++ public/js/ponk.js
@@ -1,55 +1,68 @@
 var ponk = angular.module("ponk", []);
 
-ponk.controller("AppCtrl", ["$scope", function($scope) {
+ponk.controller("AppCtrl", ["$scope", "$http", function($scope, $http) {
   var pk = this;
 
+  pk.board = {};
+  $http.get('/board').success(function(data){
+    pk.board = data[0];
+    console.log(pk.board);
+  });
+
+
+
   //dummy data
-  pk.modules = [
-    {
-      type: "text",
-      content: "this is a text field"
-    },
-    {
-      type: "youtube",
-      content: "address to youtubevideo"
-    }
-    ];
+  // pk.modules = [
+  //   {
+  //     wType: "text",
+  //     content: "this is a text field"
+  //   },
+  //   {
+  //     wType: "youtube",
+  //     content: "address to youtubevideo"
+  //   }
+  //   ];
+  //
+  pk.showAddModule = false;
 
-    pk.showAddModule = false;
-
-    pk.newModule = {
-      type: "text",
-      content: ""
-    }
+  pk.newModule = {
+    wType: "text",
+    content: ""
+  }
 
   pk.toggleAddModule = function () {
            pk.showAddModule = !pk.showAddModule;
   };
 
   pk.addModule = function() {
-      pk.modules.push(pk.newModule);
-      pk.newModule = { type: "text", };
-      pk.showAddModule = false;
+    pk.board.widgets.push(pk.newModule);
+    pk.newModule = { wType: "text", };
+    pk.showAddModule = false;
+
+    $http.post('/board', pk.board)
+      .success(function() {
+
+      });
   };
 
   pk.deleteModule = function(module) {
-      var index = pk.modules.indexOf(module);
+      var index = pk.board.widgets.indexOf(module);
       if(index != -1) {
-        pk.modules.splice(index, 1);
+        pk.board.widgets.splice(index, 1);
       }
   };
 
 }]);
 
 ponk.directive("module", function($compile) {
-  var before = '<div><span class="glyphicon glyphicon-remove-circle pointer" ng-click="deleteModule(module)"></span></div>';
-  var textTemplate = '<div class=" col-xs-2">' + before + '<div class="module">{{ module.content }}</div></div>';
+  var before = '<div><span class="glyphicon glyphicon-remove-circle pointer" ng-click="deleteModule(widget)"></span></div>';
+  var textTemplate = '<div class=" col-xs-2">' + before + '<div class="module">{{ widget.content }}</div></div>';
   var youtubeTemplate = '<div class="col-xs-4">' + before + '<div class="module"><iframe id="ytplayer" type="text/html" width="500" height="300" src="http://www.youtube.com/embed/?autoplay=0" frameborder="0"/></div></div>';
 
-  var getTemplate = function(type) {
+  var getTemplate = function(wType) {
 
     var template = "";
-    switch(type) {
+    switch(wType) {
       case "youtube":
         template = youtubeTemplate;
         break;
@@ -62,15 +75,15 @@ ponk.directive("module", function($compile) {
   }
 
   var linker = function(scope, element, attrs) {
-    var el = $compile(getTemplate(scope.module.type))(scope);
+    var el = $compile(getTemplate(scope.widget.wType))(scope);
     element.replaceWith(el);
-    scope.deleteModule = function(m){
-      scope.removeFn({module:m});
+    scope.deleteModule = function(w){
+      scope.removeFn({widget:w});
     }
   }
 
   return {
-    scope: {module: '=', removeFn:'&'},
+    scope: {widget: '=', removeFn:'&'},
     transclude: 'true',
     controller: 'AppCtrl',
     restrict: 'E',
